6043256b4f823b48c2405ee6e14b4424d9ee9d2c
tinapgaara <yt2443@columbia.edu>
2015-10-15 10:29:51 -0400

    maxPathsum

---
+++ src/tree/MaxPathSum.java
@@ -0,0 +1,26 @@
+package tree;
+
+/**
+ * Created by max2 on 10/9/15.
+ */
+/*
+* Leetcode:
+*
+* Given a binary tree, find the maximum path sum.
+
+    For this problem, a path is defined as any sequence of nodes from some starting node to any node in the
+    tree along the parent-child connections. The path does not need to go through the root.
+
+    For example:
+    Given the below binary tree,
+        1
+      / \
+     2   3
+     Return 6.
+*
+* */
+public class MaxPathSum {
+    public int maxPathSum(TreeNode root) {
+
+    }
+}
