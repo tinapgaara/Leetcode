47f5c1e19ee32ea6ba4029b5e33bb5a7e1ab0e30
Ying Tan <yt2443@columbia.edu>
2015-10-17 01:39:11 -0400

    calculator

--- src/bloomberg/Calculator.java
+++ src/bloomberg/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
     public int calculate(String expression) {
         char[] chs = expression.toCharArray();
         Stack<String> stack = new Stack<String>();
-        stack.push("$");
+        stack.push("(");
         String operator = "";
         for (int i = 0; i < chs.length; i++) {
             int digit = chs[i] - '0';
@@ -26,7 +26,13 @@ public class Calculator {
                 if (operator.length() > 0) {
                     if (!stack.isEmpty()) {
                         String operation = stack.peek();
-                        if (operation.equals("*")) {
+                        if (chs[i] == ')') {
+                            stack.push(operator);
+                            stack.push(recurCal(stack, "(") + "");
+                            operator = "";
+                            continue;
+                        }
+                        else if (operation.equals("*")) {
                             stack.pop();
                             if (!stack.isEmpty()) {
                                 int operator2 = Integer.parseInt(stack.pop());
@@ -40,10 +46,6 @@ public class Calculator {
                                 stack.push((Integer.parseInt(operator) / operator2) + "");
                             }
 
-                        } else if (operation.equals(")")) {
-                            stack.pop();
-                            // iteration to do calculation until find "("
-                            stack.push(recurCal(stack, "(") + "");
                         } else {
                             stack.push(operator);
                         }
@@ -53,32 +55,59 @@ public class Calculator {
                 stack.push(chs[i] + "");
             }
         }
-        return recurCal(stack, "$");
+        if (operator.length() > 0) stack.push(operator);
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res = recurCal(stack, "(");
+            if (!stack.isEmpty()) {
+                stack.push(res+"");
+            }
+        }
+        return res;
     }
 
     public int recurCal(Stack<String> stack, String terminator) {
         if (!stack.isEmpty()) {
-            int operator1 = Integer.parseInt(stack.pop());
-            if (!stack.isEmpty()) {
-                String op = stack.pop();
-                if (op.equals(terminator)) return operator1;
-                if (op.equals("+")) stack.push(operator1 + recurCal(stack, terminator) + "");
-                else if (op.equals("-")) stack.push(recurCal(stack, terminator) - operator1 + "");
-                else if (op.equals("*")) {
+            String str = stack.pop();
+            if (str.equals(")")) return recurCal(stack, terminator);
+            else {
+                int nInt = Integer.parseInt(str);
+                if ((nInt >= 0)) {
+                    System.out.println(str);
+                    int operator1 = nInt;
                     if (!stack.isEmpty()) {
-                        int operator2 = Integer.parseInt(stack.pop());
-                        stack.push(operator2 * operator1 + "");
-                        recurCal(stack, terminator);
-                    }
-                } else if (op.equals("/")) {
-                    if (!stack.isEmpty()) {
-                        int operator2 = Integer.parseInt(stack.pop());
-                        stack.push(operator2 / operator1 + "");
-                        recurCal(stack, terminator);
+                        String op = stack.pop();
+                        if (op.equals(terminator)) return operator1;
+                        if (op.equals("+")) {
+                            int val = operator1 + recurCal(stack, terminator);
+                            //stack.push(val + "");
+                            return val;
+                        } else if (op.equals("-")) {
+                            int val = recurCal(stack, terminator) - operator1;
+                            //stack.push(val + "");
+                            return val;
+                        } else if (op.equals("*")) {
+                            if (!stack.isEmpty()) {
+                                int operator2 = Integer.parseInt(stack.pop());
+                                stack.push(operator2 * operator1 + "");
+                                return recurCal(stack, terminator);
+                            }
+                        } else if (op.equals("/")) {
+                            if (!stack.isEmpty()) {
+                                int operator2 = Integer.parseInt(stack.pop());
+                                stack.push((operator2 / operator1) + "");
+                                return recurCal(stack, terminator);
+                            }
+                        }
                     }
                 }
             }
         }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        Calculator ob = new Calculator();
+        ob.calculate("(2+3*4/(1+2/1))");
     }
 }
