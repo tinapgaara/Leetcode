f6c7b8e946c0844b661b8c527f166ac7a33c8f05
Ying Tan <yt2443@columbia.edu>
2015-11-03 09:04:39 -0500

    search in rotated array

--- src/google/mathpro/PerfectSquares.java
+++ src/google/mathpro/PerfectSquares.java
@@ -12,4 +12,26 @@ package google.mathpro;
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 * */
 public class PerfectSquares {
+
+    public int numSquares(int n) {
+
+        int[] minNum = new int[n+1];
+        for (int i = 1 ; i * i <= n ; i ++) {
+            minNum[i * i] = 1;
+        }
+        for (int i = 2; i <= n; i ++) {
+            if (minNum[i] == 1) continue;
+            minNum[i] = n;
+            for (int j = 1; j * j <= i; j ++) {
+                int prevValue = j * j;
+                minNum[i] = Math.min(minNum[i], minNum[prevValue] + minNum[i - prevValue]);
+            }
+        }
+        return minNum[n];
+    }
+
+    public static void main(String[] args) {
+        PerfectSquares ob = new PerfectSquares();
+        System.out.println(ob.numSquares(13));
+    }
 }
